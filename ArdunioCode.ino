391b90fe525fce3477e758dd7157325b22910069
closeplanet2 <filerjosh@hotmail.com>
2020-12-20 03:34:52 +0000

    Send Back 1
    Added the code to send back what liquid is stored in the pump variables

--- ArdunioCode.ino
+++ ArdunioCode.ino
@@ -25,4 +25,23 @@ void recieveMessage(){
   char msg[2];
   msg[0] = my_serial.read();
   msg[1] = my_serial.read();
+
+  if(msg[0] == 'A'){
+    CreatePumpMessage(pumpOneLquid,pumpTwoLquid,pumpThreeLquid,pumpFourLquid,pumpFiveLquid,pumpSixLquid);
+  }
 }
+
+void sendMessage(const char *data, int l){
+  byte len[4];
+  len[0] = 85; //preamble
+  len[1] = 85; //preamble
+  len[2] = (l >> 8) & 0x000000FF;
+  len[3] = (l & 0x000000FF);
+  my_serial.write(len, 4);
+  my_serial.flush();
+  my_serial.write(data);
+  my_serial.flush();
+}
+
+//A = Send back what liquids are set in each pump
+//a = set the liquids which are in each pump
--- DataBuilder.ino
+++ DataBuilder.ino
@@ -1,1 +1,32 @@
+void CreatePumpMessage(String one, String two, String three, String four, String five, String six ){
+  char charBufone[one.length()];
+  char charBuftwo[two.length()];
+  char charBufthree[three.length()];
+  char charBuffour[four.length()];
+  char charBuffive[five.length()];
+  char charBufsix[six.length()];
+  
+  one.toCharArray(charBufone, one.length());
+  two.toCharArray(charBuftwo, two.length());
+  three.toCharArray(charBufthree, three.length());
+  four.toCharArray(charBuffour, four.length());
+  five.toCharArray(charBuffive, five.length());
+  six.toCharArray(charBufsix, six.length());
 
+  int size = one.length() + 4 + two.length() + 4 + three.length() + 4 + four.length() + 4 + five.length() + 4 + six.length() + 4;
+  char dest[size];
+  strcpy(dest, charBufone);
+  strcat(dest, ";");
+  strcpy(dest, charBuftwo);
+  strcat(dest, ";");
+  strcpy(dest, charBufthree);
+  strcat(dest, ";");
+  strcpy(dest, charBuffour);
+  strcat(dest, ";");
+  strcpy(dest, charBuffive);
+  strcat(dest, ";");
+  strcpy(dest, charBufsix);
+  strcat(dest, ";");
+
+  sendMessage(dest,size);
+}
